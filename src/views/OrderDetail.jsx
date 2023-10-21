28d0578e08ad7535d3b156ef02db2ceaa8099ced
josealexanderdrt <josealexanderdrt@gmail.com>
2023-10-21 11:38:49 -0300

    Resultado final

--- src/views/OrderDetail.jsx
+++ src/views/OrderDetail.jsx
@@ -1,51 +1,59 @@
-import { PizzaContext } from '../context/PizzaProvider'
+import { PizzaContext } from "../context/PizzaProvider";
 import { OrderDetailContext } from "../context/OrderDetailProvider";
-import { useContext,useEffect } from "react";
+import { useContext, useEffect } from "react";
 import { Card, Button, Container } from "react-bootstrap";
 
 const OrderDetail = () => {
-  const { pizzas, setPizzas } = useContext(PizzaContext);
-  const { orderList, addToOrderDetail,minusToOrderDetail, totalOrder} = useContext(OrderDetailContext);
+  const { formatNumber } = useContext(PizzaContext);
+  const { orderList, addToOrderDetail, minusToOrderDetail, totalOrder } =
+    useContext(OrderDetailContext);
 
   return (
     <>
-      <div className="gallery grid-columns-5 p-3">
+      <Container >
+        <h3>Detalle del Pedido</h3>
         {orderList.map((pizza, i) => (
-          <div key={i}>
-            <div style={{ backgroundImage: `url(${pizza.img})` }}>
-              sss
-            </div>
-          <div>
-            <h1>{pizza.name}</h1>
-            <div>
-              {pizza.price*pizza.quantity}
-            </div>
-            <div>
-              {pizza.quantity}
-            </div>       
-          </div>
-          <Button
-                    variant="dark"
-                    className="m-1 p-1"
-                    onClick={() => addToOrderDetail(pizza.id)}
-                  >
-                    +
-                  </Button>
-                  <Button
-                    variant="dark"
-                    className="m-1 p-1"
-                    onClick={() => minusToOrderDetail(pizza.id)}
-                  >
-                    -
-                  </Button>
-          </div>
+          <section key={i} className="boxOrderDetail row-gap">
+            <figure
+              style={{ backgroundImage: `url(${pizza.img})` }}
+              className="boxOrderDetaiImg m-1"
+            ></figure>
+            <article className="m-1"> 
+              <h4>{pizza.name}</h4>
+              <h6>El Precio de esta pizza es:{" "} {formatNumber(pizza.price)}</h6>
+            </article>
+            
+            
+            <Button
+              variant="dark"
+              className="m-1 p-1 wbutton"
+              onClick={() => addToOrderDetail(pizza.id)}
+            >
+              +
+            </Button>
+            <div className="text-center">
+              <h6>Und</h6>
+              <h6>{pizza.quantity}</h6></div>
+
+              <div>
+                
+                {formatNumber(pizza.price * pizza.quantity)}</div>
+            <Button
+              variant="dark"
+              className="m-1 p-1 wbutton"
+              onClick={() => minusToOrderDetail(pizza.id)}
+            >
+              -
+            </Button>
+          </section>
         ))}
-        Total: {totalOrder}
-      </div>
-     
-      
+        <section>
+          <h3>Total</h3>
+        <h3>{formatNumber(totalOrder)}</h3> 
+        </section>
+      </Container>
     </>
   );
-}
+};
 
-export default OrderDetail
+export default OrderDetail;
